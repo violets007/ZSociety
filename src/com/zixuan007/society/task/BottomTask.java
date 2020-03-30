ceefe05326f0b0bab2efcab3c4f5b22dcaf033bc
lt-name <572413378@qq.com>
2020-03-30 15:29:43 +0800

    整理部分文件

--- src/com/zixuan007/society/task/BottomTask.java
+++ src/com/zixuan007/society/task/BottomTask.java
@@ -1,35 +1,26 @@
-/*    */ package com.zixuan007.society.task;
-/*    */ 
-/*    */ import cn.nukkit.Player;
-/*    */ import cn.nukkit.scheduler.Task;
-/*    */ import com.zixuan007.society.SocietyPlugin;
-/*    */ import com.zixuan007.society.utils.SocietyUtils;
-/*    */ import java.util.Collection;
-/*    */ 
-/*    */ public class BottomTask
-/*    */   extends Task {
-/* 11 */   private SocietyPlugin societyPlugin = null;
-/*    */   
-/*    */   public BottomTask(SocietyPlugin societyPlugin) {
-/* 14 */     this.societyPlugin = societyPlugin;
-/*    */   }
-/*    */ 
-/*    */   
-/*    */   public void onRun(int i) {
-/* 19 */     Boolean isShowTip = (Boolean)this.societyPlugin.getConfig().get("是否开启底部");
-/* 20 */     if (isShowTip.booleanValue()) {
-/* 21 */       Collection<Player> players = this.societyPlugin.getServer().getOnlinePlayers().values();
-/* 22 */       String tipText = (String)SocietyPlugin.getInstance().getConfig().get("tipText");
-/* 23 */       for (Player player : players) {
-/* 24 */         tipText = SocietyUtils.formatButtomText(tipText, player);
-/* 25 */         player.sendTip(tipText);
-/*    */       } 
-/*    */     } 
-/*    */   }
-/*    */ }
+package com.zixuan007.society.task;
+
+import cn.nukkit.Player;
+import cn.nukkit.scheduler.PluginTask;
+import cn.nukkit.scheduler.Task;
+import com.zixuan007.society.SocietyPlugin;
+import com.zixuan007.society.utils.SocietyUtils;
+import java.util.Collection;
 
 
-/* Location:              D:\下载\ZSociety-1.0.3alpha.jar!\com\zixuan007\society\task\BottomTask.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
+public class BottomTask extends PluginTask<SocietyPlugin> {
+
+    public BottomTask(SocietyPlugin owner) {
+        super(owner);
+    }
+
+    public void onRun(int i) {
+        Collection<Player> players = owner.getServer().getOnlinePlayers().values();
+        String tipText = SocietyPlugin.getInstance().getConfig().getString("tipText", null);
+        for (Player player : players) {
+            tipText = SocietyUtils.formatButtomText(tipText, player);
+            player.sendTip(tipText);
+        }
+    }
+
+}
