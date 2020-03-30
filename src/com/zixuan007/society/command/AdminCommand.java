851ff4c55bc3921f2852013ad9e3d182ca51890b
lt-name <572413378@qq.com>
2020-03-30 10:37:22 +0800

    command优化

--- src/com/zixuan007/society/command/AdminCommand.java
+++ src/com/zixuan007/society/command/AdminCommand.java
@@ -1,26 +1,14 @@
-/*    */ package com.zixuan007.society.command;
-/*    */ 
-/*    */ import cn.nukkit.command.Command;
-/*    */ import cn.nukkit.command.CommandSender;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ public class AdminCommand
-/*    */   extends Command
-/*    */ {
-/*    */   public AdminCommand(String name) {
-/* 13 */     super(name);
-/*    */   }
-/*    */ 
-/*    */   
-/*    */   public boolean execute(CommandSender commandSender, String s, String[] strings) {
-/* 18 */     return false;
-/*    */   }
-/*    */ }
+package com.zixuan007.society.command;
 
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
 
-/* Location:              D:\下载\ZSociety-1.0.3alpha.jar!\com\zixuan007\society\command\AdminCommand.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
+public class AdminCommand extends Command {
+    public AdminCommand(String name) {
+        super(name);
+    }
+
+    public boolean execute(CommandSender commandSender, String s, String[] strings) {
+        return false;
+    }
+}
--- src/com/zixuan007/society/command/MainCommand.java
+++ src/com/zixuan007/society/command/MainCommand.java
@@ -1,44 +1,34 @@
-/*    */ package com.zixuan007.society.command;
-/*    */ 
-/*    */ import cn.nukkit.Player;
-/*    */ import cn.nukkit.command.Command;
-/*    */ import cn.nukkit.command.CommandSender;
-/*    */ import cn.nukkit.form.window.FormWindow;
-/*    */ import com.zixuan007.society.SocietyPlugin;
-/*    */ import com.zixuan007.society.window.WindowManager;
-/*    */ import com.zixuan007.society.window.society.SocietyWindow;
-/*    */ 
-/*    */ 
-/*    */ public class MainCommand
-/*    */   extends Command
-/*    */ {
-/* 15 */   private SocietyPlugin societyPlugin = SocietyPlugin.getInstance();
-/*    */   
-/*    */   public MainCommand() {
-/* 18 */     super((String)SocietyPlugin.getInstance().getConfig().get("公会主命令"), "公会插件总命令", "/§b公会 §e帮助");
-/* 19 */     setPermission("");
-/* 20 */     getCommandParameters().clear();
-/*    */   }
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */   
-/*    */   public boolean execute(CommandSender sender, String commandName, String[] args) {
-/* 33 */     if (sender instanceof cn.nukkit.command.ConsoleCommandSender) {
-/* 34 */       sender.sendMessage("§c进制控制台输入命令");
-/* 35 */       return false;
-/*    */     } 
-/* 37 */     Player player = (Player)sender;
-/* 38 */     if (commandName.equals(getName())) {
-/* 39 */       SocietyWindow societyWindow = WindowManager.getSocietyWindow(player);
-/* 40 */       player.showFormWindow((FormWindow)societyWindow);
-/*    */     } 
-/* 42 */     return false;
-/*    */   }
-/*    */ }
+package com.zixuan007.society.command;
+
+import cn.nukkit.Player;
+import cn.nukkit.command.Command;
+import cn.nukkit.command.CommandSender;
+import cn.nukkit.command.ConsoleCommandSender;
+import com.zixuan007.society.SocietyPlugin;
+import com.zixuan007.society.window.WindowManager;
+import com.zixuan007.society.window.society.SocietyWindow;
+
+public class MainCommand extends Command {
+    private SocietyPlugin societyPlugin = SocietyPlugin.getInstance();
+
+    public MainCommand() {
+        super((String)SocietyPlugin.getInstance().getConfig().get("公会主命令"), "公会插件总命令", "/§b公会 §e帮助");
+        this.setPermission("");
+        this.getCommandParameters().clear();
+    }
+
+    public boolean execute(CommandSender sender, String commandName, String[] args) {
+        if (sender instanceof ConsoleCommandSender) {
+            sender.sendMessage("§c进制控制台输入命令");
+            return false;
+        } else {
+            Player player = (Player)sender;
+            if (commandName.equals(this.getName())) {
+                SocietyWindow societyWindow = WindowManager.getSocietyWindow(player);
+                player.showFormWindow(societyWindow);
+            }
+
+            return false;
+        }
+    }
+}
